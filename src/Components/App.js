3765c4ff58c52005a72af88104b10dcd377f297a
Thomas McNish <mcnishthomas@gmail.com>
2022-05-22 12:31:09 -0400

    added SSL link for api key

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,7 +14,7 @@ function App() {
   const performSearch = (value) => setQuery(value);
 
   useEffect(() => {
-    axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=F6dbdsD9RXRBIUX9HzVTt7sjbcUYTE3l`)
+    axios(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=F6dbdsD9RXRBIUX9HzVTt7sjbcUYTE3l`)
       .then(response => setData(response.data.data))
       .catch(error => console.log('Error fetching and parsing data', error))
       .finally(() => setIsLoading(false));
